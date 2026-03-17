09e822da53e67f8df4f592117693de19cdb44aec
santhan <santhan.v@tickingminds.com>
2026-03-17 14:19:56 +0530

    changed issue in pacs.009 cov, added agents too in that

--- get_raw_msg.py
+++
@@ -1,7 +0,0 @@
-
-from lxml import etree
-xsd_str = '<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"><xs:element name="Cd"><xs:simpleType><xs:restriction base="xs:string"><xs:maxLength value="4"/></xs:restriction></xs:simpleType></xs:element></xs:schema>'
-xsd = etree.XMLSchema(etree.fromstring(xsd_str))
-doc = etree.fromstring('<Cd>Passport</Cd>')
-xsd.validate(doc)
-print(f"RAW: {xsd.error_log[0].message}")
